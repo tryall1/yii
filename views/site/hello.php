9a1974b5870ef851af8d2edb105b533e6578d9b6
tryall <genial712@gmail.com>
2016-06-28 22:42:05 +0300

    first-commit

---
+++ views/site/hello.php
@@ -0,0 +1,8 @@
+<?php
+
+use yii\helpers\Html;
+$this->title = 'Ну и мразь же ты';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<h1><?= Html::encode($this->title) ?></h1>
+<h2>Привет даун</h2>
---
+++ views/site/index.php
@@ -0,0 +1,51 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Добро пожаловать';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Зашибенно!</h1>
+
+        <p class="lead">Ты крут и точка.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Начнём</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Заголовок</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+
+            </div>
+            <div class="col-lg-4">
+                <h2>Заголовок</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Заголовок</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+            </div>
+        </div>
+
+    </div>
+</div>
