29a8687820e77387209fa221c4ebfa763bf99262
tryall <genial712@gmail.com>
2016-07-10 12:36:28 +0300

    add comments and category

--- modules/admin/views/category/index.php
+++ modules/admin/views/category/index.php
@@ -6,28 +6,24 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Посты';
+$this->title = 'Blog Categories';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="blog-post-index">
+<div class="blog-category-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Создать пост', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create Blog Category', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'columns' => [
-          //  ['class' => 'yii\grid\SerialColumn'],
+            ['class' => 'yii\grid\SerialColumn'],
 
             'id',
-            'title',
-            //'content:ntext',
-            'category_id',
-            'status',
-            // 'created_at',
-            // 'updated_at',
+            'name',
+            'description',
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
--- modules/admin/views/comments/index.php
+++ modules/admin/views/comments/index.php
@@ -6,28 +6,27 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Посты';
+$this->title = 'Blog Comments';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="blog-post-index">
+<div class="blog-comment-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Создать пост', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create Blog Comment', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'columns' => [
-          //  ['class' => 'yii\grid\SerialColumn'],
+            ['class' => 'yii\grid\SerialColumn'],
 
             'id',
-            'title',
-            //'content:ntext',
-            'category_id',
-            'status',
-            // 'created_at',
-            // 'updated_at',
+            'author',
+            'email:email',
+            'url:url',
+            'content:ntext',
+            // 'status',
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
